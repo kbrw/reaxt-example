bb5cd40b12ba26708d61fe1be226043893a72ceb
Arnaud Wetzel <arnaud.wetzel@kbrwadventure.com>
2015-02-02 23:21:08 +0100

    More explicit dyn handler example: react-router

--- lib/reaxt_example.ex
+++ lib/reaxt_example.ex
@@ -29,13 +29,24 @@ defmodule ReaxtExample do
   </head>
   <body>
     <h1> Reaxt application example </h1>
-    <div id="content"><%= render.html %></div>
+    <div style="background-color: #BBB; padding: 20px"><h4 style="color: #FFF">Rendering area</h4>
+      <div id="content"><%= render.html %></div>
+    </div>
+    <h2>Targeted module is directly a react component</h2>
     <ul>
       <li><a href="/">Root app with subcomponent and state</a></li>
-      <li><a href="/subcomponent">Render a subcomponent alone</a></li>
+      <li><a href="/subcomponent">Render a subcomponent alone with css</a></li>
+    </ul>
+    <h2>Targeted module implements reaxt_server_render and reaxt_client_render (here react-router)</h2>
+    <ul>
+      <li><a href="/myrouter/routed-app">First Link rendered and routed by react-router</a></li>
+      <li><a href="/myrouter/routed-css-app">Second Link rendered and routed by react-router</a></li>
+      <li><a href="/myrouter/toto">routed by React router, no route match, default handler</a></li>
+    </ul>
+    <h2>Error management</h2>
+    <ul>
       <li><a href="/with_stacktrace">JS error with stacktrace</a></li>
       <li><a href="/without_stacktrace">JS error without stacktrace</a></li>
-      <li><a href="/dyn_handler">Dynamic handler renderer</a></li>
     </ul>
     <script src="http://fb.me/react-0.12.2.js"></script>
     <script src="/public/<%= WebPack.file_of(:main) %>"></script>
@@ -58,12 +69,13 @@ defmodule ReaxtExample do
   get "/subcomponent", do:
     send_rendered_resp(conn,{:multi_components,:with_css})
 
-  get "/dyn_handler" do
-    render = Reaxt.render!(:dyn_comp,%{items: ["Home","Contact","Pages"]})
+  get "/myrouter/*_rest" do
+    ## this part use react_router as an example of dynamic handler selection
+    render = Reaxt.render!(:my_router,full_path(conn))
     send_resp(conn, 200,EEx.eval_string(@layout,render: render))
   end
 
   match _ do
-    send_resp(conn, 404, "oops")
+    send_resp(conn, 404,"Ooops, 404")
   end
 end
